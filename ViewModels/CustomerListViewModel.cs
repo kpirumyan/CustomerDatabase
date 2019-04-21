4e6c74c1a9ea77a759430990e9bb2b831be7ebc5
Karen Pirumyan <pir-karen@yandex.ru>
2019-04-21 21:54:38 +0400

    Creates CustomerListViewModel

---
+++ ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CustomerDatabase.Models;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CustomerDatabase.ViewModels
+{
+  public class CustomerListViewModel : INotifyPropertyChanged
+  {
+    #region Fields
+    private Customer _customer;
+    #endregion    
+
+    #region Constructors
+
+    public CustomerListViewModel()
+    {
+      Customer = new Customer();
+    }
+    #endregion
+
+    #region DependencyProperties
+    public Customer Customer
+    {
+      get => _customer;
+
+      set
+      {
+        _customer = value;
+        NotifyPropertyChanged();
+      }
+    }
+    #endregion
+
+    #region INotifyPropertyChanged
+
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    #endregion
+  }
+}
--- Views/CustomerListView.xaml.cs
+++ Views/CustomerListView.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CustomerDatabase.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace CustomerDatabase.Views
 {
@@ -22,6 +11,7 @@ namespace CustomerDatabase.Views
     public CustomerListView()
     {
       InitializeComponent();
+      this.DataContext = new CustomerListViewModel();
     }
   }
 }
