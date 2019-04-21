2dcd4d09be218c7c87aec8385fb53d0ef7fa3050
Karen Pirumyan <pir-karen@yandex.ru>
2019-04-21 21:09:24 +0400

    Creates models and view

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerDatabase.Models
+{
+  public struct Customer
+  {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+    public Sex Sex { get; set; }
+    public string PassportId { get; set; }
+  }
+}
